815e1d26105e510a4fb1e9a39ce26c43f185ec76
martin3572 <70953403+martin3572@users.noreply.github.com>
2021-04-25 12:58:37 +0200

    Add files via upload

--- SemPracaTWW/js/main.js
+++ SemPracaTWW/js/main.js
@@ -6,14 +6,39 @@ var body = document.getElementsByTagName("body");
 var del = document.querySelector('.btn-danger');
 //getting the length of localstorage and then setting new IDs
 var historyLength = localStorage.length;
-newOrigID = historyLength+1;
-newTransID = historyLength+2;
 
-var rowCompleted = 0;
+newID = historyLength+1;
+newTimeID = historyLength+2;
+function TimeElapsed(time){
+    var currentTime = new Date()
+    var elapsedTime = currentTime.getTime() - time;
+    var seconds = elapsedTime/1000;
+    console.log(seconds);
+    switch(true){
+        case seconds<=5: 
+            return "Just now";
+        case seconds<=60:
+            return "< 1 Minute ago";
+        case seconds<=300:
+            return "< 5 Minutes ago";
+        case seconds<=1800:
+            return "< 30 Minutes ago";
+        case seconds <=3600:
+            return "< 1 Hour ago";
+        case seconds <=21600:
+            return "< 6 Hours ago";
+        case seconds <=86400:
+            return "Today";
+        default:
+            return "> 1 Day ago";
+    }
+}
+
 //display all existing data
-function loadTable(){ 
+function LoadTable(){ 
     var table = document.getElementById('myTable');
-    var tbodyRef = document.getElementById('tbody')[0];
+    var tbodyRef = document.getElementsByClassName('tbody')[0];      
+   
     for(var i = 1; i<= historyLength;i++){
         console.log(i);
         if(i%2!=0){    
@@ -23,47 +48,93 @@ function loadTable(){
             cell.appendChild(cellText);
             row.appendChild(cell);
         }
+        else{
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode(TimeElapsed(localStorage.getItem(i)));
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+            tbodyRef.prepend(row);
+            table.prepend(tbodyRef);
+        }     
     }
-};
+}
+
+function LoadLastSearched(){
+    var table = document.getElementById('countryTable');
+    var tbodyRef = document.getElementsByClassName('tbody')[0];
+    
+    for(var i = 1; i<= historyLength;i++){
+        console.log(i);
+        if(i%2!=0){    
+            var row = document.createElement("tr");
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode(localStorage.getItem(i));
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        else{
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode(TimeElapsed(localStorage.getItem(i)));
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+            tbodyRef.prepend(row);
+            table.prepend(tbodyRef);
+        }     
+    }
+}
 
 del.onclick = function(){
     localStorage.clear();
     window.location.reload();
 }
 search.onclick = function () {
-    console.log(userInput.value);
     var inputText = userInput.value;
-    GetData(inputText);
-    // REST API url endpoint
-    
+    if(inputText = ""){
+        alert("Empty");
+    }else{
+        GetData(inputText);
+        AddToHistory();
+    }   
 }
+
 async function GetData(countryName){
+    // REST API url endpoint
     var api_url = 'https://restcountries.eu/rest/v2/name/'+ countryName;
     console.log.api_url;
     const response = await fetch(api_url)
     const data = await response.json();
-    console.log(data);
-    const {name, population, currency} = data[0];
-    var text = document.createTextNode(population);
-    var body = document.getElementById("tbody");
-    body.appendChild(text);
+    // let keyArray = Object.keys(data); 
+    // // keyArray = ['a','b','c']
+    // let valuesArray = Object.values(data);
+    // // valuesArray = ['kitten', 'puppy', 'lion'];
+    const {name, population, currency, region, capital, flag} = data[0];
+
+    //var text = document.createTextNode(population);
+    //var body = document.getElementsByClassName("tbody")[0];
+    //body.appendChild(text);
+    var time = new Date();
     //creating new records to localstorage
-    localStorage.setItem(newOrigID,countryName);
+    localStorage.setItem(newID,countryName);
+    localStorage.setItem(newTimeID, time.getTime());
     // hide the loading dialog
+    
+}
+
+function AddToHistory(){
     loadingDiv.style.display = 'none';
-
     // enable translate button
-    //button.removeAttribute('disabled');
-    var table = document.getElementsByClassName('myTable');
+    var table = document.getElementById('myTable');
     var tbodyRef = document.getElementsByClassName('tbody')[0];
-
     // Insert a row at the end of table
     var row = document.createElement("tr");
     var cell = document.createElement("td");
-    var cellText = document.createTextNode(localStorage.getItem(newOrigID));
+    var cellText = document.createTextNode(localStorage.getItem(newID));
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(TimeElapsed(localStorage.getItem(newTimeID)));
     cell.appendChild(cellText);
     row.appendChild(cell);
     tbodyRef.prepend(row);
     table.prepend(tbodyRef);
-    loadingDiv.style.display = 'invisible';
 }
